2211657d183a25c01b30ecf79cc1561a69d73b6a
Ashish Kumar <ag.ashishaggarwal@gmail.com>
2020-02-06 23:23:10 +0530

    Updates

---
+++ precache-manifest.895799a14cb3d8aac51ee3a4594aad27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15b8777f04f99d99d82b3bb4d3f01fdd",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "5c43f09d4ccf237f0171",
+    "url": "/monster-rolodex/static/css/main.b77da1bc.chunk.css"
+  },
+  {
+    "revision": "803ee1468e32a5bd4b4c",
+    "url": "/monster-rolodex/static/js/2.3b2b3505.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monster-rolodex/static/js/2.3b2b3505.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c43f09d4ccf237f0171",
+    "url": "/monster-rolodex/static/js/main.806a9af0.chunk.js"
+  },
+  {
+    "revision": "85cb2c00df0e1ad7d48d",
+    "url": "/monster-rolodex/static/js/runtime-main.3e508d1c.js"
+  }
+]);
